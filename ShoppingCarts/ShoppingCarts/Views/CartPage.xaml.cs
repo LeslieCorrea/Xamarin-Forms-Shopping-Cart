215c10abd1b852e1a1b5d720d26e2e007039c930
LESLIE CORREA <correaleslie69@yahoo.com>
2018-02-28 23:29:14 +0530

    Code re-factor commit

--- ShoppingCarts/ShoppingCarts/Views/CartPage.xaml.cs
+++ ShoppingCarts/ShoppingCarts/Views/CartPage.xaml.cs
@@ -1,10 +1,5 @@
 ﻿using ShoppingCarts.Model;
 using ShoppingCarts.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
