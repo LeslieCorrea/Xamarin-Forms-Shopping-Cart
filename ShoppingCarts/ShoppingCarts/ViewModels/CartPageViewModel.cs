1d6061778786e37a40d8dc48c7aef65dc72d1ba2
LESLIE CORREA <correaleslie69@yahoo.com>
2018-02-28 22:32:05 +0530

    Updated ViewModel commit

--- ShoppingCarts/ShoppingCarts/ViewModels/CartPageViewModel.cs
+++ ShoppingCarts/ShoppingCarts/ViewModels/CartPageViewModel.cs
@@ -1,11 +1,46 @@
-﻿using MvvmHelpers;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MvvmHelpers;
+using ShoppingCarts.Model;
+using ShoppingCarts.Services.ServiceImplementation;
+using Xamarin.Forms;
 
 namespace ShoppingCarts.ViewModels
 {
     public class CartPageViewModel : BaseViewModel
     {
+        public ObservableRangeCollection<Item> ShoppingItems { get; set; }
+
+        public Command GetData { get; set; }
+
+        public readonly IItemService _Service;
+
         public CartPageViewModel()
         {
+            ShoppingItems = new ObservableRangeCollection<Item>();
+            GetData = new Command(async () => await GetDataCommand());
+            _Service = DependencyService.Get<IItemService>();
+        }
+
+        private async Task GetDataCommand()
+        {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+                IEnumerable<Item> ItemsList = await _Service.GetItems();
+                ShoppingItems.ReplaceRange(ItemsList);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception is : " + e);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
