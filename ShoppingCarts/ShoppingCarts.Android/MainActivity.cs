7234aadb41e9e5b5c89cc7ecccf876f189795754
LESLIE CORREA <correaleslie69@yahoo.com>
2018-04-25 22:36:27 +0530

    Added splash screen for android commit

--- ShoppingCarts/ShoppingCarts.Android/MainActivity.cs
+++ ShoppingCarts/ShoppingCarts.Android/MainActivity.cs
@@ -9,11 +9,16 @@ using Android.OS;
 
 namespace ShoppingCarts.Droid
 {
-    [Activity(Label = "ShoppingCarts", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ShoppingCarts", Icon = "@drawable/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            // Changing to App's theme since we are OnCreate and we are ready to
+            // "hide" the splash
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            base.SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -24,4 +29,3 @@ namespace ShoppingCarts.Droid
         }
     }
 }
-
--- ShoppingCarts/ShoppingCarts.Android/Resources/Resource.designer.cs
+++ ShoppingCarts/ShoppingCarts.Android/Resources/Resource.designer.cs
@@ -2192,26 +2192,26 @@ namespace ShoppingCarts.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_1 = 2130837778;
-			
 			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_2 = 2130837779;
+			public const int avd_hide_password_1 = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_3 = 2130837780;
+			public const int avd_hide_password_2 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_hide_password_3 = 2130837781;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_1 = 2130837781;
-			
 			// aapt resource value: 0x7f020116
-			public const int avd_show_password_2 = 2130837782;
+			public const int avd_show_password_1 = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_3 = 2130837783;
+			public const int avd_show_password_2 = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int avd_show_password_3 = 2130837784;
 			
 			// aapt resource value: 0x7f020055
 			public const int Cart = 2130837589;
@@ -2765,11 +2765,11 @@ namespace ShoppingCarts.Droid
 			// aapt resource value: 0x7f02010c
 			public const int notification_icon_background = 2130837772;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_bg = 2130837776;
-			
 			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_low_bg = 2130837777;
+			public const int notification_template_icon_bg = 2130837777;
+			
+			// aapt resource value: 0x7f020112
+			public const int notification_template_icon_low_bg = 2130837778;
 			
 			// aapt resource value: 0x7f02010d
 			public const int notification_tile_bg = 2130837773;
@@ -2779,6 +2779,9 @@ namespace ShoppingCarts.Droid
 			
 			// aapt resource value: 0x7f02010f
 			public const int slideout = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int splash = 2130837776;
 			
 			static Drawable()
 			{
@@ -4735,6 +4738,9 @@ namespace ShoppingCarts.Droid
 			
 			// aapt resource value: 0x7f0b0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131427334;
+			
+			// aapt resource value: 0x7f0b018c
+			public const int Theme_Splash = 2131427724;
 			
 			// aapt resource value: 0x7f0b011f
 			public const int ThemeOverlay_AppCompat = 2131427615;
