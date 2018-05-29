684bd691475e1025b209b278195a15efef7c9369
LESLIE CORREA <correaleslie69@yahoo.com>
2018-05-29 22:05:27 +0530

    Added different images commit

--- ShoppingCarts/ShoppingCarts/Services/ServiceImplementation/ItemService.cs
+++ ShoppingCarts/ShoppingCarts/Services/ServiceImplementation/ItemService.cs
@@ -24,7 +24,7 @@ namespace ShoppingCarts.Services.ServiceImplementation
 
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "http://pngimg.com/uploads/shopping_cart/shopping_cart_PNG37.png",
                 Name = "Item1",
                 Index = 1,
                 Status = Settings.ItemStatus1
@@ -38,35 +38,35 @@ namespace ShoppingCarts.Services.ServiceImplementation
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759712_jar_512x512.png",
                 Name = "Item3",
                 Index = 3,
                 Status = Settings.ItemStatus3
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/11/15/853021_jar_512x512.png",
                 Name = "Item4",
                 Index = 4,
                 Status = Settings.ItemStatus4
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2017/01/23/874953_alcohol_512x512.png",
                 Name = "Item5",
                 Index = 5,
                 Status = Settings.ItemStatus5
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759662_food_512x512.png",
                 Name = "Item6",
                 Index = 6,
                 Status = Settings.ItemStatus6
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759686_food_512x512.png",
                 Name = "Item7",
                 Index = 7,
                 Status = Settings.ItemStatus7
@@ -80,49 +80,49 @@ namespace ShoppingCarts.Services.ServiceImplementation
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "http://pngimg.com/uploads/shopping_cart/shopping_cart_PNG37.png",
                 Name = "Item9",
                 Index = 9,
                 Status = Settings.ItemStatus9
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759686_food_512x512.png",
                 Name = "Item10",
                 Index = 10,
                 Status = Settings.ItemStatus10
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/03/759418_food_512x512.png",
                 Name = "Item11",
                 Index = 11,
                 Status = Settings.ItemStatus11
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759662_food_512x512.png",
                 Name = "Item12",
                 Index = 12,
                 Status = Settings.ItemStatus12
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/11/15/853021_jar_512x512.png",
                 Name = "Item13",
                 Index = 13,
                 Status = Settings.ItemStatus13
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/03/759418_food_512x512.png",
                 Name = "Item14",
                 Index = 14,
                 Status = Settings.ItemStatus14
             });
             Items.Add(new Item
             {
-                Image = "Items",
+                Image = "https://www.shareicon.net/data/128x128/2016/05/04/759629_wine_512x512.png",
                 Name = "Item15",
                 Index = 15,
                 Status = Settings.ItemStatus15
