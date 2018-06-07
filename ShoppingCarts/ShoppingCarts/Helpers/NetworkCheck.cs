29ce8c9d45bc383098ba5245825d39fd7729f98a
LESLIE CORREA <correaleslie69@yahoo.com>
2018-06-07 14:46:55 +0530

    Added Network check and related nuget packages commit

---
+++ ShoppingCarts/ShoppingCarts/Helpers/NetworkCheck.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.Connectivity;
+
+namespace ShoppingCarts.Helpers
+{
+    public class NetworkCheck
+    {
+        public static bool IsInternet()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
