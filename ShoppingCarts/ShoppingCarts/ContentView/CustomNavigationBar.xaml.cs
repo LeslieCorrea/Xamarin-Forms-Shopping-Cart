97bab4bd8f6e950195c85e02221688c0e9e53bd0
LESLIE CORREA <correaleslie69@yahoo.com>
2018-06-13 12:00:51 +0530

    Code refactor commit

--- ShoppingCarts/ShoppingCarts/ContentView/CustomNavigationBar.xaml.cs
+++ ShoppingCarts/ShoppingCarts/ContentView/CustomNavigationBar.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using ShoppingCarts.Views;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
