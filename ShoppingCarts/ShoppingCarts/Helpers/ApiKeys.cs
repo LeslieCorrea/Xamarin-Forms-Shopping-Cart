00b5157a8fada5992dcf3fba14c3846f923e9632
LESLIE CORREA <correaleslie69@yahoo.com>
2018-06-18 13:07:32 +0530

    Added app center api key commit

---
+++ ShoppingCarts/ShoppingCarts/Helpers/ApiKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCarts.Helpers
+{
+    public static class ApiKeys
+    {
+        public static readonly string AndroidAppCenterKey = "Place your key here";
+
+        public static readonly string iOSAppCenterKey = "Place your key here";
+    }
+}
