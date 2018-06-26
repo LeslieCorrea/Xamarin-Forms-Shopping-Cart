17ed1a81ee815a18ded06a66b1be4e68072065dc
LESLIE CORREA <correaleslie69@yahoo.com>
2018-06-26 15:55:02 +0530

    Added image in master page commit

--- ShoppingCarts/ShoppingCarts/Model/MasterPageItem.cs
+++ ShoppingCarts/ShoppingCarts/Model/MasterPageItem.cs
@@ -5,7 +5,7 @@ namespace ShoppingCarts.Model
     public class MasterPageItem
     {
         public string Title { get; set; }
-
         public Type TargetType { get; set; }
+        public string ImageName { get; set; }
     }
 }
