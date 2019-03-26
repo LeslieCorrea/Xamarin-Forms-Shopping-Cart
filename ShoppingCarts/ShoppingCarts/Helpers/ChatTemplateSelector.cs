f29c78c31dcdb08811a622cc3fd7f04ffc2145c6
LESLIE CORREA <correaleslie69@yahoo.com>
2019-03-26 21:32:15 +0530

    Added ChatTemplateSelector commit

---
+++ ShoppingCarts/ShoppingCarts/Helpers/ChatTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using ShoppingCarts.Model;
+using ShoppingCarts.Views;
+using Xamarin.Forms;
+
+namespace ShoppingCarts.Helpers
+{
+    internal class ChatTemplateSelector : DataTemplateSelector
+    {
+        private DataTemplate incomingDataTemplate;
+        private DataTemplate outgoingDataTemplate;
+
+        public ChatTemplateSelector()
+        {
+            this.incomingDataTemplate = new DataTemplate(typeof(IncomingMessageViewCell));
+            this.outgoingDataTemplate = new DataTemplate(typeof(OutgoingMessageViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var messageVm = item as Message;
+            if (messageVm == null)
+                return null;
+
+            return (messageVm.User == App.User) ? outgoingDataTemplate : incomingDataTemplate;
+        }
+    }
+}
