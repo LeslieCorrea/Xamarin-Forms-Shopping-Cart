e0b85ace92b3dd9c465e4965870a853c1c3fd396
LESLIE CORREA <correaleslie69@yahoo.com>
2019-03-26 20:04:37 +0530

    Added incoming and outgoing message ui commit

---
+++ ShoppingCarts/ShoppingCarts/Views/IncomingMessageViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoppingCarts.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IncomingMessageViewCell : ViewCell
+    {
+        public IncomingMessageViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
