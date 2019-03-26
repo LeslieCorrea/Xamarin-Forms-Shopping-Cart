10e60e2fe6702db04f125af1e161808b31361dca
LESLIE CORREA <correaleslie69@yahoo.com>
2019-03-26 19:53:37 +0530

    Added alert in chat page commit

--- ShoppingCarts/ShoppingCarts/Views/ChatPage.xaml.cs
+++ ShoppingCarts/ShoppingCarts/Views/ChatPage.xaml.cs
@@ -9,12 +9,23 @@ using Xamarin.Forms.Xaml;
 
 namespace ShoppingCarts.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ChatPage : ContentPage
-	{
-		public ChatPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatPage : ContentPage
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ShowAlert();
+        }
+
+        public void ShowAlert()
+        {
+            Application.Current.MainPage.DisplayAlert("Note:", "Chat with customer care executive here", "OK");
+        }
+    }
 }
